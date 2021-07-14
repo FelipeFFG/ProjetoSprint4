9c479b957417b2c92bef4a7e7ad2d8446eb30c7a
FelipeFFG <felipeffggomes@hotmail.com>
2021-07-14 20:09:21 -0300

    Otimizando os Imports da classe PessoaController

--- src/main/java/com/example/projetosprint4/controller/PessoaController.java
+++ src/main/java/com/example/projetosprint4/controller/PessoaController.java
@@ -1,20 +1,17 @@
 package com.example.projetosprint4.controller;
 
-import com.example.projetosprint4.controller.dto.EnderecoDto;
 import com.example.projetosprint4.controller.dto.PessoaDto;
-import com.example.projetosprint4.controller.form.PessoaForm;
 import com.example.projetosprint4.model.Pessoa;
 import com.example.projetosprint4.repository.EnderecoRepository;
 import com.example.projetosprint4.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,31 +25,26 @@ public class PessoaController {
     private PessoaRepository pessoaRepository;
 
 
-
-
-
-
     //Buscar Todas as pessoas
     @GetMapping
-    public ResponseEntity<?> buscarTodasAsPessoas(){
+    public ResponseEntity<?> buscarTodasAsPessoas() {
         List<Pessoa> TodasAsPessoas = pessoaRepository.findAll();
-        if (TodasAsPessoas !=null){
+        if (TodasAsPessoas != null) {
             return new ResponseEntity<>(new PessoaDto()
                     .convertePessoasparaPessoasDtoComEnderecoDto(TodasAsPessoas), HttpStatus.OK);
-        }else
-            return new ResponseEntity<>("Nao foi possivel achar nenhuma pessoa",HttpStatus.BAD_REQUEST);
+        } else
+            return ResponseEntity.notFound().build();
     }
-
 
 
     //Buscar Por ID
     @GetMapping("/{id}")
-    public ResponseEntity<?> BuscarPorId(@PathVariable Long id){
+    public ResponseEntity<?> BuscarPorId(@PathVariable Long id) {
         Optional<Pessoa> TodasAsPessoas = pessoaRepository.findById(id);
-        if (TodasAsPessoas.isPresent()){
-            return  ResponseEntity.ok(new PessoaDto(TodasAsPessoas.get()));
-        }else
-            return new ResponseEntity<>("Nao foi possivel achar nenhuma pessoa",HttpStatus.BAD_REQUEST);
+        if (TodasAsPessoas.isPresent()) {
+            return ResponseEntity.ok(new PessoaDto(TodasAsPessoas.get()));
+        } else
+            return ResponseEntity.notFound().build();
     }
 
 
